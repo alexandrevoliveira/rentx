4966539b2c08e28a79e47bdd33b5b48f8b92883f
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-07-27 22:51:23 -0300

    Validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,27 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
-const categoriesRoutes = Router()
+const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description })
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" }); 
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 })
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list()
+  const all = categoriesRepository.list();
 
-  return response.json(all)
+  return response.json(all);
 })
 
 export { categoriesRoutes };
