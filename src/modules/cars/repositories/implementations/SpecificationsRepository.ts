2bb64f9a3a06b103d2a6216d7ea87a0df7ca47db
Alexandre <alexandrevoliveira30@gmail.com>
2021-08-08 16:10:27 -0300

    Criando migration de especificacao

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { getRepository, Repository } from "typeorm";
+
 import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationDTO,
@@ -5,33 +7,26 @@ import {
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
   async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-    const specification = new Specification();
+    const specification = this.repository.create({ name, description });
 
-    Object.assign(specification, {
-      name,
-      description,
-      created_at: Date.now(),
-    });
-
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
   async findByName(name: string): Promise<Specification> {
-    const specification = this.specifications.find(
-      (spec) => spec.name === name
-    );
+    const specification = this.repository.findOne({ name });
     return specification;
   }
 
   async list(): Promise<Specification[]> {
-    return this.specifications;
+    const specifications = this.repository.find();
+    return specifications;
   }
 }
 
