d6ed342a384d44ca4e9f2b7a0b21813b39870585
Alexandre <alexandrevoliveira30@gmail.com>
2021-08-09 18:42:01 -0300

    Autenticacao nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "71b0fa02271be0266376eb107428046d"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
+  } catch (err) {
+    throw new Error("Token invalid");
+  }
+}
