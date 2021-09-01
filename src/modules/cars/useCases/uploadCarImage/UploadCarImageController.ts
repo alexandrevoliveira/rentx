8b22ae5892fbd4ec9c0799d48fd324704ebaba37
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-09-01 10:42:10 -0300

    Caso de uso do cadastro de imagens do carro

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++
@@ -1,3 +0,0 @@
-class UploadCarImageController {}
-
-export { UploadCarImageController };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { container } from "tsyringe";
 
@@ -25,3 +27,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository
+);
